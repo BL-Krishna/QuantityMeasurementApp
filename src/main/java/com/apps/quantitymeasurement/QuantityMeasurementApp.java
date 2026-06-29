4788e7e4343d84023d7a2c73db4a6b66ccae0c5b
BL-Krishna <krishna.chandana@bridgelabz.com>
2026-06-29 11:37:14 +0530

    UC11: Volume Measurement Equality, Conversion, and Addition (Litre, Millilitre, Gallon)

--- src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
+++ src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
@@ -2,9 +2,6 @@ package com.apps.quantitymeasurement;
 
 public class QuantityMeasurementApp {
 
-
-
-    // Generic Equality Demonstration
     public static <U extends IMeasurable>
     void demonstrateEquality(
             Quantity<U> first,
@@ -20,7 +17,6 @@ public class QuantityMeasurementApp {
         System.out.println();
     }
 
-    // Generic Conversion Demonstration
     public static <U extends IMeasurable>
     void demonstrateConversion(
             Quantity<U> quantity,
@@ -36,7 +32,6 @@ public class QuantityMeasurementApp {
         System.out.println();
     }
 
-    // Generic Addition Demonstration
     public static <U extends IMeasurable>
     void demonstrateAddition(
             Quantity<U> first,
@@ -56,89 +51,61 @@ public class QuantityMeasurementApp {
 
     public static void main(String[] args) {
 
-        QuantityWeight kg =
-                new QuantityWeight(
-                        1,
-                        WeightUnit.KILOGRAM);
-
-        QuantityWeight gram =
-                new QuantityWeight(
-                        1000,
-                        WeightUnit.GRAM);
-
-        QuantityWeight pound =
-                new QuantityWeight(
-                        2.20462,
-                        WeightUnit.POUND);
-
-        System.out.println(kg.equals(gram));
-
-        System.out.println(
-                pound.convertTo(
-                        WeightUnit.KILOGRAM));
-
-        System.out.println(
-                kg.add(gram));
-
-        System.out.println(
-                kg.add(
-                        gram,
-                        WeightUnit.GRAM));
-
-        // Length
-        Quantity<LengthUnit> feet =
+        // Volume Quantities
+        Quantity<VolumeUnit> litre =
                 new Quantity<>(
                         1.0,
-                        LengthUnit.FEET
+                        VolumeUnit.LITRE
                 );
 
-        Quantity<LengthUnit> inches =
-                new Quantity<>(
-                        12.0,
-                        LengthUnit.INCHES
-                );
-
-        demonstrateEquality(feet, inches);
-
-        demonstrateConversion(
-                feet,
-                LengthUnit.INCHES
-        );
-
-        demonstrateAddition(
-                feet,
-                inches,
-                LengthUnit.FEET
-        );
-
-        // Weight
-        Quantity<WeightUnit> kilogram =
-                new Quantity<>(
-                        1.0,
-                        WeightUnit.KILOGRAM
-                );
-
-        Quantity<WeightUnit> gram =
+        Quantity<VolumeUnit> millilitre =
                 new Quantity<>(
                         1000.0,
-                        WeightUnit.GRAM
+                        VolumeUnit.MILLILITRE
                 );
 
+        Quantity<VolumeUnit> gallon =
+                new Quantity<>(
+                        1.0,
+                        VolumeUnit.GALLON
+                );
+
+        // Equality
         demonstrateEquality(
-                kilogram,
-                gram
+                litre,
+                millilitre
+        );
+
+        demonstrateEquality(
+                gallon,
+                new Quantity<>(
+                        3.78541,
+                        VolumeUnit.LITRE
+                )
+        );
+
+        // Conversion
+        demonstrateConversion(
+                litre,
+                VolumeUnit.MILLILITRE
         );
 
         demonstrateConversion(
-                kilogram,
-                WeightUnit.GRAM
+                gallon,
+                VolumeUnit.LITRE
+        );
+
+        // Addition
+        demonstrateAddition(
+                litre,
+                millilitre,
+                VolumeUnit.LITRE
         );
 
         demonstrateAddition(
-                kilogram,
-                gram,
-                WeightUnit.KILOGRAM
+                gallon,
+                litre,
+                VolumeUnit.GALLON
         );
     }
-
 }
